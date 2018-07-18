0fa4371e6af9dac0983bab3789ceb33ef71a5283
caitl <caitl@10.65.116.121>
2018-07-18 16:55:30 -0700

    7/18/18

--- src/BodyPartQuiz.java
+++ src/BodyPartQuiz.java
@@ -1,3 +1,4 @@
+
 /*
  *    Copyright (c) The League of Amazing Programmers 2013-2017
  *    Level 1
@@ -15,50 +16,94 @@ import javax.swing.JPanel;
 
 public class BodyPartQuiz {
 
-	// 0. You can use the celebrity photos we have placed in the default java package,  
-	// or if you prefer, get celebrity photos from the Internet, place them in the default
+	// 0. You can use the celebrity photos we have placed in the default java
+	// package,
+	// or if you prefer, get celebrity photos from the Internet, place them in the
+	// default
 	// package, and change the names below.
-	
+
 	String firstImage = "src/arnold.jpeg";
 	String secondImage = "src/leonardo.jpeg";
 	String thirdImage = "src/morgan.jpeg";
 	String fourthImage = "src/jack.jpeg";
-	
+
 	JFrame window = new JFrame();
 	JPanel panel = new JPanel();
-
 
 	private void startQuiz() {
 
 		// 1. Make an int variable to hold the score.
-
+		int score = 0;
 		// 2. Set the size of the window in the initializeGui() method below
 
 		for (int i = 0; i < 4; i++) {
-			
+
 			// 4. Ask the user who this person is and store their answer
-			String guess= JOptionPane.showInputDialog("who is this?");
-			
+			String guess = JOptionPane.showInputDialog("who is dis?");
+
 			// 5. Check their answer. If they guessed correctly:
 			// -- Tell them they are right and increase the score by 1
 
+			// Arnold
+
+			if (i == 0) {
+				if (guess .equals("Arnold")) {
+					JOptionPane.showMessageDialog(null, "Dat's correct! U has gained uno point");
+					score++;
+				} 
+				else {
+					JOptionPane.showMessageDialog(null, "U is wrong");
+				}
+			}
+			
+			else if (i == 1) {
+				if (guess.equals("Leonardo")) {
+					JOptionPane.showMessageDialog(null, "Dat's correct! U has gained uno point");
+					score++;
+				} 
+				else {
+					JOptionPane.showMessageDialog(null, "U is wrong");
+				}
+			}
+			
+			
+			else if (i == 2) {
+				if (guess.equals("Morgan")) {
+					JOptionPane.showMessageDialog(null, "Dat's correct! U has gained uno point");
+					score++;
+				} 
+				else {
+					JOptionPane.showMessageDialog(null, "U is wrong");
+				}
+			}
+			
+			
+			else if (i == 3) {
+				if (guess.equals("Jack")) {
+					JOptionPane.showMessageDialog(null, "Dat's correct! U has gained uno point");
+					score++;
+				} 
+				else {
+					JOptionPane.showMessageDialog(null, "U is wrong");
+				}
+			}
 			// 6. Otherwise:
 			// -- Tell them they are wrong and who the person is
-
+			JOptionPane.showMessageDialog(null, "You has " + score + " points!");
 			// 7. Use the showNextImage() method below to get the next image
 			showNextImage();
-		    // 8. Show them their current score
+			// 8. Show them their current score
 			
 			// 9. .... repeat for all your images.....
 
-
+			
 		}
 
 	}
 
 	public void showNextImage() {
 		panel.removeAll();
-		panel.add(getNextImage());		
+		panel.add(getNextImage());
 		window.setVisible(true);
 	}
 
@@ -73,12 +118,12 @@ public class BodyPartQuiz {
 		imageIterator = imageList.iterator();
 		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		window.add(panel);
-		
-		// 3. Change the size of the window so that you can only see part of the image.		
-		window.setSize(500,500);
-		
+
+		// 3. Change the size of the window so that you can only see part of the image.
+		window.setSize(150, 150);
+
 		showNextImage();
-		
+
 	}
 
 	private JLabel loadImage(String fileName) {
